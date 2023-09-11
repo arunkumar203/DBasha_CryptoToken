c4c4109e0061dadf841e8d59c49eb3b14260d7f3
arunkumar203 <98168395+arunkumar203@users.noreply.github.com>
2023-09-11 11:54:34 +0530

    Add files via upload

---
+++ src/token_assets/src/components/Faucet.jsx
@@ -0,0 +1,38 @@
+import React,{ useState } from "react";
+import {  token  } from "../../../declarations/token";
+
+function Faucet() {
+
+  const [isDisabled, setDisable]=useState(false);
+  const [buttonText,setText]=useState("Gimme gimme");
+
+  async function handleClick(event) {
+    setDisable(true);
+    const result= await token.payOut();
+    setText(result);
+   // setDisable(false);
+
+  }
+
+  return (
+    <div className="blue window">
+      <h2>
+        <span role="img" aria-label="tap emoji">
+          🚰
+        </span>
+        Faucet
+      </h2>
+      <label>Get your free baasha tokens here! Claim 10,000 baasha tokens to your account.</label>
+      <p className="trade-buttons">
+        <button id="btn-payout"
+         onClick={handleClick}
+         disabled={isDisabled}
+         >
+          {buttonText}
+        </button>
+      </p>
+    </div>
+  );
+}
+
+export default Faucet;
